9f1d4980a5600e1f48c846c0f24e0d037a696add
yepo <aurelien.flura@gmail.com>
2020-06-23 19:04:48 +0200

    debut accueil

--- web/app/themes/DClab/inc/query/post.php
+++ web/app/themes/DClab/inc/query/post.php
@@ -6,12 +6,17 @@
 <?php
 add_action('pre_get_posts', function (WP_Query $query){
   if(is_admin() ||
-  !$query->is_main_query() ||
-  (!is_home() && !is_search() && !is_category())
+  !$query->is_main_query()
+  // || (!is_home() && !is_search() && !is_category())
   ){
     return;
   }
   $query->set('posts_per_page', 20);
+    if(
+    is_post_type_archive('forum')
+    ){
+  dump('coucou');}
 });
+
 
 ?>
